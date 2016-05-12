ebbe39656eb518dd62ed5436be73a976e3a789ab
Sergio Aguayo <sergioag@qmailhosting.net>
2016-05-12 16:05:46 -0500

    Agregada persistencia de valores

--- app.rb
+++ app.rb
@@ -3,5 +3,17 @@ require 'sinatra'
 enable :sessions
 
 get '/' do
+    @cells = [
+        [nil,2,nil],
+        [1,nil,2],
+        [nil,nil,3]
+    ];
+    erb :sudoku
+end
+
+post '/sudoku' do
+    @cells = params["cells"]
+    puts "Parametros:"
+    puts @cells
     erb :sudoku
 end
