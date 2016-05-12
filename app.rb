e84287d5476fee4ff38cf9e6d904428e6d52c468
Sergio Aguayo <sergioag@qmailhosting.net>
2016-05-12 17:08:15 -0500

    Agregadas pruebas de Cucumber

--- app.rb
+++ app.rb
@@ -3,11 +3,24 @@ require 'sinatra'
 enable :sessions
 
 get '/' do
-    @cells = [
-        [nil,2,nil],
-        [1,nil,2],
-        [nil,nil,3]
-    ];
+    @cells = {
+        "0"=>{
+            "0" => nil,
+            "1" => 2,
+            "2" => nil
+        },
+        "1"=>{
+            "0" => 1,
+            "1" => nil,
+            "2" => 2
+        },
+        "2"=>{
+            "0" => nil,
+            "1" => nil,
+            "2" => 3
+        }
+    }
+    puts @cells
     erb :sudoku
 end
 
