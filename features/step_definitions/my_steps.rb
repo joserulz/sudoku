0cf8e9e7de8b2e8aa72e610133879ed7383687b9
Sergio Aguayo <sergioag@qmailhosting.net>
2016-05-13 10:15:29 -0500

    Agregado escenario de victoria

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -3,10 +3,14 @@ Given(/^I visit the homepage$/) do
 end
 
 When(/^I enter "([^"]*)" in row (\d+), column (\d+)$/) do |value, row, column|
+    row = (row.to_i) - 1
+    column = (column.to_i) - 1
     fill_in("cells[" + row.to_s + "][" + column.to_s + "]", :with => value)
 end
 
 Then(/^I should see "([^"]*)" in row (\d+), column (\d+)$/) do |value, row, column|
+    row = (row.to_i) - 1
+    column = (column.to_i) - 1
     response_body.should have_selector("input[name='cells[" + row.to_s + "][" + column.to_s + "]']/@value") do |element|
         element.should contain(value)
     end
--- features/sudoku.feature
+++ features/sudoku.feature
@@ -9,3 +9,12 @@ Feature: Sudoku
         When I enter "3" in row 1, column 1
         And I press "Jugar"
         Then I should see "3" in row 1, column 1
+
+    Scenario: Llenar el tablero correctamente, ver mensaje de ganador
+        Given I visit the homepage
+        When I enter "3" in row 1, column 1
+        And I enter "1" in row 1, column 3
+        And I enter "3" in row 2, column 2
+        And I enter "2" in row 3, column 1
+        And I enter "1" in row 3, column 2
+        Then I should see "Ganaste!"
